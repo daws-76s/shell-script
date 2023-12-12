3494e30fd1f42279546cc97128ad71ad0bb00c31
joindevopscloud <info@joindevops.com>
2023-12-12 08:27:13 +0530

    added 2nd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Please enter your username"
+echo "Please enter your username:: "
 
-read USERNAME #the value entered above will be automatically atteched to USERNAME variable
+read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
 echo "Username is: $USERNAME"
