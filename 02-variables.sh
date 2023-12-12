20b6e4c066fec1c93adec2c9ece4dfbeaa3e7e9d
joindevopscloud <info@joindevops.com>
2023-12-12 08:05:01 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello Suresh, Good Morning"
-echo "Suresh: Hi Ramesh, very Good Morning"
-echo "Ramesh: How are you Suresh?"
-echo "Suresh: I am good Ramesh, How are you"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
