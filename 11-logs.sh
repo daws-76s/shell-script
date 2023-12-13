8f691124f0030256e4fd8db3ec0c6b61642c5c7c
joindevopscloud <info@joindevops.com>
2023-12-13 08:35:44 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,22 +2,25 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ... FAILED"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo "You are root user"
