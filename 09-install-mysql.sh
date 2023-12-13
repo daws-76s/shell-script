03a5c6b2e26628440f1ec62454314562dd1a2b77
joindevopscloud <info@joindevops.com>
2023-12-13 07:49:57 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -10,7 +10,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysqlll -y
+yum install mysql -y
 
 if [ $? -ne 0 ]
 then
@@ -18,4 +18,14 @@ then
     exit 1
 else
     echo "Installing MySQL is SUCCESS"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
 fi
