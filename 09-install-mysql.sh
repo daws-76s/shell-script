5105c2d8be10f6d38933b9d20c0b0e13eeb0042b
joindevopscloud <info@joindevops.com>
2023-12-13 07:48:04 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -11,3 +11,11 @@ else
 fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
