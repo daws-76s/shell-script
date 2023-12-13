1180a20abf66414a2429dfd1d630280da6a04071
joindevopscloud <info@joindevops.com>
2023-12-13 08:11:39 +0530

    added 2nd script

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
