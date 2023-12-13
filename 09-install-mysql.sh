b1969b5f0785a0da081a22d163ebaddf04cc4c7d
joindevopscloud <info@joindevops.com>
2023-12-13 07:45:42 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,8 +5,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
 else
     echo "You are root user"
-fi
+fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
