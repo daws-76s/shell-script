6e0d58442b7b4aff609f4e02984dfaae89d07f77
joindevopscloud <info@joindevops.com>
2023-12-18 08:06:30 +0530

    daws-76s

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -10,8 +10,8 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage"
+        message+="High Disk Usage on $partition: $usage\n"
     fi
 done <<< $DISK_USAGE
 
-echo "Message: $message"
+echo -e "Message: $message"
