e419c4233e9c1fbf78ec83c0fd8f7f0846ea1925
joindevopscloud <info@joindevops.com>
2023-12-18 07:36:20 +0530

    daws-76s

---
+++ 15-delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
+then
+    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
+fi
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+done <<< $FILES_TO_DELETE
