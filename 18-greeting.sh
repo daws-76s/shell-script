93cb70fe2b6a743ccfc798fb143c79ebecf6e214
joindevopscloud <info@joindevops.com>
2023-12-19 07:41:12 +0530

    daws-76s

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -15,7 +15,8 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        h|*) USAGE; exit;;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
         :) USAGE; exit;;
+        h) USAGE; exit;;
     esac
 done
