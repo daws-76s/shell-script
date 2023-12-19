662fa837ef505bb6dffa9eb82a7abd600e6d50db
joindevopscloud <info@joindevops.com>
2023-12-19 07:36:34 +0530

    daws-76s

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "Hello $PERSON, Good Morning. We are learning shell script"
+USAGE(){
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -h, Display Help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        h|*) USAGE; exit;;
+    esac
+done
