4d1107b661bf3bae0f4ff9d663207d02b4357b6b
joindevopscloud <info@joindevops.com>
2023-12-19 07:44:38 +0530

    daws-76s

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -20,3 +20,11 @@ while getopts ":n:w:h" opt; do
         h) USAGE; exit;;
     esac
 done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+    echo "ERROR: Both -n and -w are mandatory options."
+    USAGE
+    exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning Shell Script."
